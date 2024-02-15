f54ca7cbf4c635c05c34bd5e4850d902ca603417
Lucas Freitas <lucaslcfjf@hotmail.com>
2024-02-15 17:37:41 -0300

    Adiciona imagem dos projetos (provisório) e monta listagem dos dados

---
+++ src/components/Projects/data.ts
@@ -0,0 +1,160 @@
+import { ProjectType } from '.';
+
+export const projectsData: ProjectType[] = [
+    {
+        id: '1',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/bestjobs/thumbnail.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/bestjobs/HomePage1.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/bestjobs/HomePage2.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/bestjobs/Login.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/bestjobs/Profile.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/bestjobs/Search.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/bestjobs/Candidacies1.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/bestjobs/Candidacies2.png',
+            },
+        ],
+    },
+    {
+        id: '2',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/bestjobs/thumbnail.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/bestjobs/HomePage1.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/bestjobs/HomePage2.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/bestjobs/Login.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/bestjobs/Profile.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/bestjobs/Search.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/bestjobs/Candidacies1.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/bestjobs/Candidacies2.png',
+            },
+        ],
+    },
+    {
+        id: '3',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/bestjobs/thumbnail.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/bestjobs/HomePage1.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/bestjobs/HomePage2.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/bestjobs/Login.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/bestjobs/Profile.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/bestjobs/Search.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/bestjobs/Candidacies1.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/bestjobs/Candidacies2.png',
+            },
+        ],
+    },
+    {
+        id: '4',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/bestjobs/thumbnail.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/bestjobs/HomePage1.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/bestjobs/HomePage2.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/bestjobs/Login.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/bestjobs/Profile.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/bestjobs/Search.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/bestjobs/Candidacies1.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/bestjobs/Candidacies2.png',
+            },
+        ],
+    },
+];
